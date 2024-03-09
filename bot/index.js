b21ed7d81f2721f15b361309004dd25f7d58f32a
dasturchioka <dasturchioka@gmail.com>
2024-03-09 15:53:59 +0500

    ready to deploy

---
+++ bot/index.js
@@ -0,0 +1,24 @@
+require("dotenv").config()
+const { Telegraf } = require("telegraf");
+const TOKEN = process.env.TELEGRAM_BOT_TOKEN;
+const bot = new Telegraf(TOKEN);
+
+const web_link = "https://celebrated-torte-184681.netlify.app/";
+
+bot.start((ctx) =>
+  ctx.reply(
+    "😊 Assalomu alaykum, taxi ilovamizga xush kelibsiz! Taxi chaqirish uchun pastdagi tugmani bosing",
+    {
+      reply_markup: {
+        keyboard: [[{ text: "Taxi chaqirish 🚕", web_app: { url: web_link } }]],
+        resize_keyboard: true,
+      },
+    }
+  )
+);
+
+bot.launch().then(() => {
+  console.log("Bot is launched!");
+});
+
+module.exports = bot;
--- index.js
+++ index.js
@@ -2,10 +2,13 @@ require("dotenv").config();
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
+require("./bot");
+
+process.setMaxListeners(0);
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, { cors: { origin: "*" } });
 
 const driver = {
   name: "Sardor Aminov",
@@ -21,7 +24,10 @@ const driver = {
 
 const prices = [5000, 6500, 4500, 10500, 13500, 4000, 6000, 14000, 12500];
 
+app.get("/", (req, res) => res.json({ msg: "Hello" }));
+
 io.on("connection", (socket) => {
+  console.log("connection");
   socket.on("clientJoined", async (data) => {
     const roomId = data.roomId;
     // har bir mijoz bilan individual shug'ullanish uchun socketda alohida `room` lar yaratiladi.
@@ -34,7 +40,7 @@ io.on("connection", (socket) => {
 
       // oxirida unga taksi borayotganini bildiramiz va shu bilan birga unga haydovchi malumotlarini ham yuboramiz
       const pickedPrice = Math.floor(Math.random() * prices.length);
-      
+
       const pickedTimeout = Math.floor(
         Math.random() * (10000 - 3000 + 1) + 3000
       );
@@ -43,6 +49,10 @@ io.on("connection", (socket) => {
         io.in(roomId).emit("driverIsGoing", { ...driver, price: pickedPrice });
       }, pickedTimeout);
     });
+  });
+
+  socket.on("disconnect", () => {
+    console.log("disconnection");
   });
 });
 
