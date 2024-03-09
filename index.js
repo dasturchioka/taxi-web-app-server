eed701a1dffeedb5a6a7a22c35ce7b6730ca853e
dasturchioka <dasturchioka@gmail.com>
2024-03-09 17:15:06 +0500

    bug is being fixed ASAP

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const http = require("http");
 const { Server } = require("socket.io");
 require("./bot");
 
-process.setMaxListeners(Infinity);
+process.setMaxListeners(0);
 
 const app = express();
 const server = http.createServer(app);
@@ -24,8 +24,6 @@ const driver = {
 
 const prices = [5000, 6500, 4500, 10500, 13500, 4000, 6000, 14000, 12500];
 
-app.get("/", (req, res) => res.json({ msg: "Hello" }));
-
 io.on("connection", (socket) => {
   console.log("connection");
   socket.on("clientJoined", async (data) => {
@@ -33,9 +31,29 @@ io.on("connection", (socket) => {
     // har bir mijoz bilan individual shug'ullanish uchun socketda alohida `room` lar yaratiladi.
     // roomId esa mijozning ilovasida ixtiyoriy raqamlar yoki harflar orqali generatsiya qilinadi.
     socket.join(roomId);
+  });
 
-    // mijoz taksi chaqiryabdi
-    socket.on("clientRequesting", async (data) => {
+  socket.on("clientRequesting", async (data) => {
+    // ! database bilan bir nimalar bo'ldi...
+
+    // oxirida unga taksi borayotganini bildiramiz va shu bilan birga unga haydovchi malumotlarini ham yuboramiz
+    const pickedPrice = Math.floor(Math.random() * prices.length);
+
+    const pickedTimeout = Math.floor(Math.random() * (10000 - 3000 + 1) + 3000);
+
+    setTimeout(async () => {
+      io.in(roomId).emit("driverIsGoing", { ...driver, price: pickedPrice });
+    }, pickedTimeout);
+  });
+
+  socket.on("leave", async () => {
+    socket.off("clientJoined", async (data) => {
+      const roomId = data.roomId;
+      // har bir mijoz bilan individual shug'ullanish uchun socketda alohida `room` lar yaratiladi.
+      // roomId esa mijozning ilovasida ixtiyoriy raqamlar yoki harflar orqali generatsiya qilinadi.
+      socket.join(roomId);
+    });
+    socket.off("clientRequesting", async (data) => {
       // ! database bilan bir nimalar bo'ldi...
 
       // oxirida unga taksi borayotganini bildiramiz va shu bilan birga unga haydovchi malumotlarini ham yuboramiz
@@ -49,6 +67,7 @@ io.on("connection", (socket) => {
         io.in(roomId).emit("driverIsGoing", { ...driver, price: pickedPrice });
       }, pickedTimeout);
     });
+    console.log("User left!");
   });
 
   socket.on("disconnect", () => {
